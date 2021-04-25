ac7c74fa155445a7733d61db2bcbf04071f204e7
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2021-04-25 10:47:05 +0700

    Update ethv100.sh

--- ethv100.sh
+++ ethv100.sh
@@ -3,16 +3,9 @@ sudo apt-get update
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 apt-get install -y git wget screen
-CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
-dpkg -i /tmp/${CUDA_REPO_PKG}
-apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
-rm -f /tmp/${CUDA_REPO_PKG}
-apt-get update
-apt-get install -y cuda-drivers
-apt-get install -y cuda
-export LD_LIBRARY_PATH=/usr/local/cuda/lib
-export PATH=$PATH:/usr/local/cuda/bin
+wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
+sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
+./NVIDIA-Linux-x86_64-460.73.01.run
 mkdir /usr/share
 mkdir /usr/share/work
 rm -r /usr/share/work/avengers
